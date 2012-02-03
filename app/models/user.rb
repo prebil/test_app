b94d89f508beae8c447309c8cb6bfced7178e760
T.J. Prebil <tj@prebil.com>
2012-02-02 20:22:13 -0600

    user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :eamil
+  attr_accessible :name, :email
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name,  :presences => true,
+                    :length    => { :maximum => 50 }
+  validates :email, :presences  => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_senstive => false }
 end
